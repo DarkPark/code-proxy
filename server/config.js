e2674e5838ee8677a152d0b2a6c130c7b19e9854
DarkPark <darkpark@pisem.net>
2014-01-30 22:43:47 +0200

    move server configuration to a separate file

---
+++ server/config.js
@@ -0,0 +1,12 @@
+/**
+ * Main server configuration
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ * @author DarkPark
+ */
+
+'use strict';
+
+module.exports = {
+	portHttp : 8800,
+	portWs   : 8900
+};
