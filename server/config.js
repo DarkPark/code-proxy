5ee1ce17540afd8945269267af496a9625ff346b
Stanislav Kalashnik <sk@infomir.eu>
2014-07-29 12:50:01 +0300

    add config description

--- server/config.js
+++ server/config.js
@@ -7,9 +7,39 @@
 'use strict';
 
 module.exports = {
-	portHttp   : 8800,
-	portWs     : 8900,
-	retryDelay : 100,
-	retryLimit : 30,
-	logging    : true
+	/**
+	 * Enables proxy server
+	 * @type {boolean}
+	 */
+	active: true,
+
+	/**
+	 * Listening HTTP port to serve proxy files
+	 * @type {number}
+	 */
+	portHttp: 8800,
+
+	/**
+	 * Listening WebSocket port to serve requests
+	 * @type {number}
+	 */
+	portWs: 8900,
+
+	/**
+	 * Time between connection/sending attempts (in ms)
+	 * @type {number}
+	 */
+	retryDelay: 100,
+
+	/**
+	 * Amount of connection/sending attempts before give up
+	 * @type {number}
+	 */
+	retryLimit: 30,
+
+	/**
+	 * Full logging
+	 * @type {boolean}
+	 */
+	logging: false
 };
